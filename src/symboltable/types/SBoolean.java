114f94f6e494cdb0efd00c94a48c5dca7da8c690
Juan Dingevan <jidingevan@gmail.com>
2023-09-15 10:24:19 -0300

    Forgot the boolean primitive type.

---
+++ src/symboltable/types/SBoolean.java
@@ -0,0 +1,6 @@
+package symboltable.types;
+
+//Same as with SFloat. We add Sumatra's initial S to
+//differentiate from Java's native Boolean class.
+public class SBoolean extends PrimitiveType{
+}
