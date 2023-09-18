31a34b053b94f310c7194748ec42008bafba4510
Juan Dingevan <jidingevan@gmail.com>
2023-09-18 11:46:44 -0300

    Refactored ReferenceType to store the referenced ype's name.

--- src/symboltable/types/ReferenceType.java
+++ src/symboltable/types/ReferenceType.java
@@ -1,4 +1,9 @@
 package symboltable.types;
 
 public class ReferenceType extends Type{
+    String referenceName;
+
+    public ReferenceType(String referenceName) {
+        this.referenceName = referenceName;
+    }
 }
