b9e2fa4e8a10e0ab2d50b681b76d509eecde6860
Juan Dingevan <jidingevan@gmail.com>
2023-10-21 15:03:42 -0300

    Added the posibility of using stage 3's user interface.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 import userinterface.ArgumentReadingUserInterface;
 import userinterface.HardwiredUserInterface;
+import userinterface.StageThreeUserInterface;
 import userinterface.UserInterface;
 
 public class Main {
     private static final UserInterface TEST_INTERFACE = new HardwiredUserInterface();
     private static final UserInterface REAL_INTERFACE = new ArgumentReadingUserInterface();
+
+    private static final UserInterface OLD_INTERFACE = new StageThreeUserInterface();
 
     public static void main(String[] args) {
         UserInterface ui = REAL_INTERFACE;
