e33d5f683ab5eb67ee7c1ca173153d7fade7fee6
Juan Dingevan <jidingevan@gmail.com>
2023-10-28 16:33:25 -0300

    Moved test cases.

---
+++ resources/AnterioresEtapas/Sentences/sinErrores/sem_chv_n_levels_of_inheritance_works.java
@@ -0,0 +1,22 @@
+///[SinErrores]
+/*class A<A1, A2> {
+    public A1 getA1() {}
+    public A2 getA2() {}
+}
+
+class B<B1, B2> extends A<B1, B2> {}
+class C<C1, C2> extends B<C1, C2> {}
+class D<D1, D2> extends C<D1, D2> {}*/
+
+class Main {
+    public static void main() {}
+
+    /*private String s;
+    private Object o;
+
+    public void m() {
+        var d = new D<String, Object>();
+        s = d.getA1();
+        o = d.getA2();
+    }*/
+}
