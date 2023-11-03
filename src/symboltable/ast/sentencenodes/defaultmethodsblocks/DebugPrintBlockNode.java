dba4e93dd32196467911b2792462e210e16b776a
Juan Dingevan <jidingevan@gmail.com>
2023-11-03 10:22:44 -0300

    Default debugPrint block now generates code.

--- src/symboltable/ast/sentencenodes/defaultmethodsblocks/DebugPrintBlockNode.java
+++ src/symboltable/ast/sentencenodes/defaultmethodsblocks/DebugPrintBlockNode.java
@@ -1,6 +1,18 @@
 package symboltable.ast.sentencenodes.defaultmethodsblocks;
 
-import symboltable.ast.sentencenodes.BlockNode;
+import codegenerator.CodeGenerator;
+import exceptions.general.CompilerException;
 
 public class DebugPrintBlockNode extends DefaultMethodBlockNode {
+    @Override
+    public void generate() throws CompilerException {
+        String c1 = " # We put the parameter i at the top of the stack (offset is hard-coded)";
+        CodeGenerator.getInstance().append("LOAD 3" + c1);
+
+        String c2 = " # We print the integer we just loaded";
+        CodeGenerator.getInstance().append("IPRINT" + c2);
+
+        String c3 = " # We print the carriage return";
+        CodeGenerator.getInstance().append("PRNLN" + c3);
+    }
 }
