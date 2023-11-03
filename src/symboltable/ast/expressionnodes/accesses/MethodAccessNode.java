3ca8a110030416746606bc72d6c2962fd71b007d
Juan Dingevan <jidingevan@gmail.com>
2023-11-03 11:11:10 -0300

    MethodAccessCall now generates code.

--- src/symboltable/ast/expressionnodes/accesses/MethodAccessNode.java
+++ src/symboltable/ast/expressionnodes/accesses/MethodAccessNode.java
@@ -1,5 +1,6 @@
 package symboltable.ast.expressionnodes.accesses;
 
+import codegenerator.CodeGenerator;
 import exceptions.general.CompilerException;
 import exceptions.semantical.sentence.DynamicUsageInStaticContextException;
 import exceptions.semantical.sentence.PrivateMemberAccessException;
@@ -17,11 +18,12 @@ import java.util.List;
 
 public class MethodAccessNode extends AccessNode {
     protected List<ExpressionNode> actualArguments;
+    protected Method method;
     @Override
     protected Type accessCheck() throws CompilerException {
         String methodName = token.getLexeme();
 
-        Method method = contextClass.getMethod(methodName);
+        method = contextClass.getMethod(methodName);
 
         if(method == null)
             throw new UnresolvedNameException(token, contextClass.getToken());
@@ -63,6 +65,72 @@ public class MethodAccessNode extends AccessNode {
 
     public void setActualArguments(List<ExpressionNode> actualArguments) {
         this.actualArguments = actualArguments;
+    }
+
+    public void generate() throws CompilerException {
+        //we partially build the called-method's RA
+        //we already have the called-method's reference in method
+        if(method.isStatic()) {
+            generateStaticCall();
+        } else {
+            generateDynamicCall();
+        }
+
+        if(hasChaining())
+            chainingNode.generate();
+    }
+
+    protected void generateDynamicCall() throws CompilerException {
+        String cThis = " # We put a reference to 'this' at the top of the stack";
+        CodeGenerator.getInstance().append("LOAD 3" + cThis);
+
+        String cSwap = " # We swap to keep the 'this' reference at the top of the stack";
+
+        if(!Type.isVoid(method.getReturnType())) {
+            String cRet = " # We reserve a memory cell for the method's return value";
+            CodeGenerator.getInstance().append("RMEM 1" + cRet);
+
+            CodeGenerator.getInstance().append("SWAP " + cSwap);
+        }
+
+        for(ExpressionNode argument : actualArguments) {
+            argument.generate();
+            CodeGenerator.getInstance().append("SWAP " + cSwap);
+        }
+
+        String cDup = " # We duplicate the 'this' reference so we don't lose it when it's used by LOADREF";
+        CodeGenerator.getInstance().append("DUP" + cDup);
+
+        String cLoad1 = " # We load the VTable (VTable offset is always 0)";
+        CodeGenerator.getInstance().append("LOADREF 0" + cLoad1);
+
+        String cLoad2 = " # We load the method's address through the VTable.";
+        CodeGenerator.getInstance().append("LOADREF " + method.getOffset() + cLoad2);
+
+        String cCall = " # We make the call.";
+        CodeGenerator.getInstance().append("CALL" + cCall);
+    }
+
+    protected void generateStaticCall() throws CompilerException {
+        //Note: this is a DYNAMIC ACCESS to a STATIC METHOD.
+        //It's different from a Dynamic-Dynamic one because we don't add
+        // a "this" reference to the RA we build.
+
+        if(!Type.isVoid(method.getReturnType())) {
+            String cRet = " # We reserve a memory cell for the method's return value";
+            CodeGenerator.getInstance().append("RMEM 1" + cRet);
+        }
+
+        for(ExpressionNode argument : actualArguments) {
+            argument.generate();
+        }
+
+        String tag = method.getTag();
+        String cTag = " # We push the static method's tag to the top of the stack";
+        CodeGenerator.getInstance().append("PUSH " + tag + cTag);
+
+        String cCall = " # We make the call.";
+        CodeGenerator.getInstance().append("CALL" + cCall);
     }
 
     @Override
