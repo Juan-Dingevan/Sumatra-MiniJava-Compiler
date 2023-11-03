7c74e2e1dfd81d7a2901fa170acb4c155d9561a7
Juan Dingevan <jidingevan@gmail.com>
2023-11-03 18:47:57 -0300

    While now generates code.

--- src/symboltable/ast/sentencenodes/WhileNode.java
+++ src/symboltable/ast/sentencenodes/WhileNode.java
@@ -1,10 +1,27 @@
 package symboltable.ast.sentencenodes;
 
+import codegenerator.CodeGenerator;
+import exceptions.general.CompilerException;
+
 public class WhileNode extends ControlStructureNode {
     public static int classID = 0;
     private final int id = classID;
     @Override
     protected int getID() {
         return id;
+    }
+
+    @Override
+    public void generate() throws CompilerException {
+        String tagIn = "while_" + id;
+        String tagOut = "out_while_" + id;
+
+        CodeGenerator.getInstance().append(tagIn + ": NOP");
+        expression.generate();
+        CodeGenerator.getInstance().append("BF " + tagOut);
+        implicitBlock.generate();
+        CodeGenerator.getInstance().append("JUMP " + tagIn);
+        CodeGenerator.getInstance().append(tagOut + ": NOP");
+
     }
 }
