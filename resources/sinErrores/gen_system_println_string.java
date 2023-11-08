1f6a5911462478a618b30b988076371e79b17087
Juan Dingevan <jidingevan@gmail.com>
2023-11-08 16:58:55 -0300

    String literals now work!

--- resources/sinErrores/gen_system_println_string.java
+++ resources/sinErrores/gen_system_println_string.java
@@ -1,7 +1,13 @@
-///Hello World&exitosamente
+///Hello World&Hola Mundo&Bonjour monde&exitosamente
 class Init {
     public static void m(){
         var s = "Hello World";
+        System.printSln(s);
+        System.printSln("Hola Mundo!");
+        m2("Bonjour monde");
+    }
+
+    public static void m2(String s) {
         System.printSln(s);
     }
 
--- src/symboltable/ast/expressionnodes/literals/StringLiteralNode.java
+++ src/symboltable/ast/expressionnodes/literals/StringLiteralNode.java
@@ -7,15 +7,30 @@ import symboltable.types.ReferenceType;
 import symboltable.types.Type;
 
 public class StringLiteralNode extends LiteralNode {
+    private static int classID = 0;
+    private int id;
+
+    public StringLiteralNode() {
+        id = classID;
+        classID++;
+    }
+
     @Override
     public Type check() throws CompilerException {
         return new ReferenceType("String");
     }
 
+    @Override
     public void generate() throws CompilerException {
         String lexeme = token.getLexeme();
-        String lexemeWithoutQuotes = lexeme.substring(1, lexeme.length()-1);
+        String tag = "str_" + id;
 
-        CodeGenerator.getInstance().append("DW " + lexeme + ",0");
+        CodeGenerator.getInstance().append(".DATA");
+        CodeGenerator.getInstance().append(tag + ": DW " + lexeme + ",0");
+
+        CodeGenerator.getInstance().append(".CODE");
+
+        String cPush = " # We leave a reference to the String we created at the top of the stack";
+        CodeGenerator.getInstance().append("PUSH " + tag + cPush);
     }
 }
